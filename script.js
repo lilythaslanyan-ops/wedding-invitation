600091a0246944ce5d30ca41aefe9f4f04dfbbd7
lilythaslanyan-ops <lilyth.aslanyan@gmail.com>
2026-06-16 01:43:35 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+    const weddingDate = new Date("July 28, 2026 00:00:00").getTime();
+
+    const timerInterval = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = weddingDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+
+        if (distance < 0) {
+            clearInterval(timerInterval);
+            document.querySelector(".timer").innerHTML = "Հարսանիքն արդեն սկսվել է!";
+        }
+    }, 1000);
